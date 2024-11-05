b0172fa11a12c038de8fbe4872ab4d220715e726
Samiksha <samikshadkamble@gmail.com>
2024-11-05 13:52:45 +0530

    Added History section

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 import axios from "axios";
 import { Search } from "neetoicons";
-import { Input, NoData } from "neetoui";
+import { Input, NoData, Typography } from "neetoui";
 import { isEmpty } from "ramda";
 
 import MovieDetailsModal from "./MovieDetailsModal";
@@ -21,6 +21,7 @@ const MovieList = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [selectedMovie, setSelectedMovie] = useState(null);
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [history, setHistory] = useState([]);
 
   const fetchMovies = async () => {
     setIsLoading(true);
@@ -52,8 +53,6 @@ const MovieList = () => {
       setData([]);
       setTotalMovieCount(0);
     }
-
-    return;
   };
 
   useEffect(() => {
@@ -65,6 +64,10 @@ const MovieList = () => {
     console.log("Movie clicked:", movie);
     setSelectedMovie(movie);
     setIsModalVisible(true);
+
+    if (!history.includes(movie.Title)) {
+      setHistory(prevHistory => [...prevHistory, movie.Title]);
+    }
   };
 
   const handleCloseModal = () => {
@@ -102,24 +105,37 @@ const MovieList = () => {
       {isEmpty(data) ? (
         <NoData className="h-full w-full" title="No products to show" />
       ) : (
-        <div className="mx-20 grid grid-cols-2 gap-x-4 gap-y-8 p-4 md:grid-cols-2 lg:grid-cols-5">
-          {/* justify-items-center */}
-          {data.map(movie => (
-            <MovieItem
-              key={movie.imdbID}
-              {...movie}
-              onClick={() => handleMovieClick(movie)}
-            />
-          ))}
+        <div className="flex">
+          <div className="ml-20 mr-2 grid grid-cols-4 gap-x-4 gap-y-8 p-4 md:grid-cols-2 lg:grid-cols-4">
+            {/* justify-items-center */}
+            {data.map(movie => (
+              <MovieItem
+                key={movie.imdbID}
+                {...movie}
+                onClick={() => handleMovieClick(movie)}
+              />
+            ))}
+          </div>
+          <div className="mr-10 w-2/6 border-l border-green-900">
+            <h1 className="mb-5 text-xl">
+              <strong>View History</strong>
+            </h1>
+            {history.map((movieName, index) => (
+              <Typography
+                className="w-100 border-1 m-2 border-red-800 bg-gray-400 px-12 py-2"
+                key={index}
+              >
+                {movieName}
+              </Typography>
+            ))}
+          </div>
         </div>
       )}
-      <div>
-        <Pagination
-          countsPerPage={DEFAULT_PAGE_SIZE}
-          setCurrentPage={setCurrentPage}
-          totalCount={totalMovieCount}
-        />
-      </div>
+      <Pagination
+        countsPerPage={DEFAULT_PAGE_SIZE}
+        setCurrentPage={setCurrentPage}
+        totalCount={totalMovieCount}
+      />
       {selectedMovie && (
         <MovieDetailsModal
           movie={selectedMovie}
